320f935c8b9a9434adf7a0c18c7117e60279add5
Xiaozhou Pu <Patrick_Pu@huntsman-ve508-0145.apn.wlan.upenn.edu>
2017-11-17 15:19:38 -0500

    [xpu] modulize to Infra, Server and Client module

--- Infra.hs
+++ Infra.hs
@@ -5,44 +5,19 @@
 
 module Infra where
 
-import qualified Data.Map as M
-import Control.Monad.State as S
 import Network.Socket as NS
 
-data Message = JoinRoom Int
-             | TextData String
-             | NickName String
-             | Disconnect
+data Message = Join Int
+             | Text String
+             | Nick String
+             | Part
+             | Quit
     deriving (Show, Eq)
 
 data Address = Address {ip :: NS.HostName, port :: NS.ServiceName}
 
-data ClientStore = ClientStore {nick :: String, room :: Int}
+readFrom :: Address -> IO Message
+readFrom addr = undefined
 
-data Chatroom = Chatroom {usr :: M.Map Address ClientStore}
-
-data ServerStore = ServerStore {grp :: M.Map Int Chatroom, getAddr :: M.Map Int Address}
-
-
--- register a server with id and addr
-registerServer :: Int -> Address -> StateT ServerStore IO ()
-registerServer n addr = do
-    server <- get
-
-    return ()
-
--- register a client with addr to grp 0 (defualt)
-registerClient :: Address -> StateT ServerStore IO ()
-registerClient = assignClient 0
-
--- assign a client to a different group
-assignClient :: Int -> Address -> StateT ServerStore IO ()
-assignClient n addr = undefined
-
--- disconnect a client from the group
-partClient :: Int -> Address -> StateT ServerStore IO ()
-partClient n addr = undefined
-
--- assign a client a nick name
-nickClient :: String -> Address -> StateT ServerStore IO ()
-nickClient nick addr = undefined
+sendTo :: Address -> Message -> IO ()
+sendTo addr msg = undefined
---
+++ Server.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE
+  TypeFamilies
+, FlexibleContexts
+#-}
+
+module Server where
+
+import Infra as Infra
+import qualified Data.Map as M
+import Control.Monad.State as S
+
+data ClientStore = ClientStore {nick :: String, room :: Int}
+
+data Chatroom = Chatroom {usr :: M.Map Infra.Address ClientStore}
+
+data ServerStore = ServerStore {grp :: M.Map Int Chatroom, getAddr :: M.Map Int Infra.Address}
+
+
+-- register a server with id and addr
+registerServer :: Int -> Infra.Address -> StateT ServerStore IO ()
+registerServer n addr = do
+    server <- get
+
+    return ()
+
+-- register a client with addr to grp 0 (defualt)
+registerClient ::Infra.Address -> StateT ServerStore IO ()
+registerClient = assignClient 0
+
+-- assign a client to a different group
+assignClient :: Int -> Infra.Address -> StateT ServerStore IO ()
+assignClient group addr = undefined
+
+-- disconnect a client from the group
+partClient :: Int -> Infra.Address -> StateT ServerStore IO ()
+partClient group addr = undefined
+
+-- assign a client a nick name
+nickClient :: String -> Infra.Address -> StateT ServerStore IO ()
+nickClient nick addr = undefined
+
+-- multicast message to all clients in the given group
+multiCastToClient :: Int -> Infra.Message -> StateT ServerStore IO ()
+multiCastToClient group msg = undefined
+
+-- multicast message to all servers (including itself)
+multiCastToServer :: Infra.Message -> StateT ServerStore IO ()
+multiCastToServer msg = undefined
+
+runServer :: IO ()
+runServer = undefined
+
+main :: IO ()
+main = runServer
