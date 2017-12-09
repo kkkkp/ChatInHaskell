d49b8e5237bafef302904705e91bace2384b1af4
Xiaozhou Pu <Patrick_Pu@Xiaozhous-MacBook-Pro.local>
2017-12-09 01:23:14 -0500

    [xpu] framework for prop_multicast_server, prop_multicast_client and prop_text_handler

--- Test.hs
+++ Test.hs
@@ -27,22 +27,23 @@ data TestStore = TestStore {
 
 type TestMonad = State TestStore
 
--- TODO: which monad should I use for test?
--- State
--- TODO: can I change the () -> a? What should I return if change to a?
+-- Test socket monad
 instance MonadSocket TestMonad Int where
-    mySend n mesg addr = do
+    mySend _ mesg addr = do
         store <- get
         let map = getMesgMap store
         put $ store {getMesgMap = M.insert addr mesg map}
 
+-- Client store Gen
 instance Arbitrary ClientStore where
     arbitrary = ClientStore <$> (arbitrary :: Gen String) <*> (arbitrary :: Gen Int)
 
+-- SockAddr Gen
 instance Arbitrary SockAddr where
     arbitrary = flip SockAddrInet host <$> port where
         port = intToPortNumber <$> (arbitrary :: Gen Int)
 
+-- Server store Gen
 instance Arbitrary ServerStore where
     arbitrary = ServerStore <$> clientInfo <*> serverInfo where
         clients    = listOf $ liftM2 (,) arbitrary arbitrary
@@ -58,7 +59,7 @@ prop_join store n addr = M.member addr map where
 f :: ServerStore -> TestStore
 f = undefined
 
--- test "+OK" response for join
+-- test response for join handler
 prop_join_handler :: Int -> Int -> SockAddr -> ServerStore -> Bool
 prop_join_handler sock n addr store = flag where
     roomID = getRoom store addr
@@ -76,7 +77,7 @@ prop_nick store name addr = name == nickName where
         Just client -> nick client
         Nothing -> name ++ "-ERR"
 
--- test "+OK" response for nick
+-- test response for nick handler
 prop_nick_handler :: Int -> SockAddr -> String -> ServerStore -> Bool
 prop_nick_handler sock addr name store = flag where
     roomID = getRoom store addr
@@ -86,16 +87,19 @@ prop_nick_handler sock addr name store = flag where
         Nothing -> "#ERR"
     flag   = if roomID /= -1 then isPrefixOf "+OK" mesg else isPrefixOf "-ERR" mesg
 
--- test "+OK" response for text
+-- test response for text handler
 prop_text_handler :: Int -> SockAddr -> String -> String -> ServerStore -> Bool
-prop_text_handler sock client text name store = True
+prop_text_handler sock client text name store = flag where
+    servers = getServer store
+    map     = getMesgMap $ execState (execStateT (textHandler sock client text name) store) $ TestStore M.empty
+    flag    = True
 
 -- test part will remove the sockaddr from the store
 prop_part :: ServerStore -> SockAddr -> Bool
 prop_part store addr = not (M.member addr map) where
     map  = getClient $ execState (partClient addr) $ store
 
--- test "+OK" response for part
+-- test response for part handler
 prop_part_handler :: Int -> SockAddr -> ServerStore -> Bool
 prop_part_handler sock addr store = flag where
     roomID = getRoom store addr
@@ -105,11 +109,19 @@ prop_part_handler sock addr store = flag where
         Nothing -> "#ERR"
     flag   = if roomID /= -1 then isPrefixOf "+OK" mesg else isPrefixOf "-ERR" mesg
 
-prop_multicast_client :: Bool
-prop_multicast_client = True
+-- test multicast_client deliver mesg to all clients
+prop_multicast_client :: Int -> Int -> String -> ServerStore -> Bool
+prop_multicast_client sock n mesg store = flag where
+    clients = getClient store
+    map     = getMesgMap $ execState (execStateT (multiCastToClient sock n mesg) store) $ TestStore M.empty
+    flag    = True
 
-prop_multicast_server :: Bool
-prop_multicast_server = True
+-- test multicast_server deliver mesg to all servers
+prop_multicast_server :: Int -> Int -> String -> ServerStore -> Bool
+prop_multicast_server sock n mesg store = flag where
+    servers = getServer store
+    map     = getMesgMap $ execState (execStateT (multiCastToServer sock n mesg) store) $ TestStore M.empty
+    flag    = True
 
 test1 :: IO ()
 test1 = quickCheck (prop_join :: ServerStore -> Int -> SockAddr -> Bool)
@@ -133,10 +145,10 @@ test7 :: IO ()
 test7 = quickCheck (prop_part_handler :: Int -> SockAddr -> ServerStore -> Bool)
 
 test8 :: IO ()
-test8 = quickCheck (prop_multicast_client :: Bool)
+test8 = quickCheck (prop_multicast_client :: Int -> Int -> String -> ServerStore -> Bool)
 
 test9 :: IO ()
-test9 = quickCheck (prop_multicast_server :: Bool)
+test9 = quickCheck (prop_multicast_server :: Int -> Int -> String -> ServerStore -> Bool)
 
 runTests :: IO ()
 runTests = test1 >> test2 >> test3 >> test4 >> test5 >> test6 >> test7 >> test8 >> test9
